38fda2e13ef4c44578d215aeb1ebc2f157bb8f84
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-10-11 15:58:58 -0400

    added seeds for employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,3 +17,14 @@ VALUES ("Sales Person", 50000, 1),
        ("Tech Enginner", 90000, 4),
        ("VP of Accounting", 225000, 5),
        ("Accountant", 75000, 5);
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Rosie", "Castro", 5, NULL),
+        ("Leo", "S", 2, 1),
+       ("Jonny", "S", 3, 1),
+       ("Sebastian", "S", 4, 3),
+       ("John", "Doe", 1, 2),
+       ("Kevin", "Castro", 6, 1),
+       ("Jorge", "C", 7, 6),
+       ("Pedro", "C", 8, 1),
+       ("Hilda", "C", 9, 8);
