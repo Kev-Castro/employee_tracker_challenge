5bf1b8449c0d32993d97e7f89584007be1c11a10
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-10-11 15:47:10 -0400

    added seeds for role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,13 +3,17 @@ USE tracker_db;
 INSERT INTO department (name)
 VALUES ("Sales"),
        ("HR"),
-       ("Admin"),
+       ("Corporate"),
        ("Tech"),
        ("Accounting");
 
-INSERT INTO department (name)
-VALUES ("Sales"),
-       ("HR"),
-       ("Admin"),
-       ("Tech"),
-       ("Accounting");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Person", 50000, 1),
+        ("Sales VP", 100000, 1),
+       ("VP of HR", 150000, 2),
+       ("HR Associate", 75000, 2),
+       ("CEO", 500000, 3),
+       ("SVP of Engineering", 180000, 4),
+       ("Tech Enginner", 90000, 4),
+       ("VP of Accounting", 225000, 5),
+       ("Accountant", 75000, 5);
